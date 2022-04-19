61e3a6cad5b55a909af6e0bab3cfd8d37f7627de
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-04-19 13:13:58 +0000

    Deployment settings

--- p5_walkthrough/settings.py
+++ p5_walkthrough/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3u7qfqfwo($*)n&ij=b9%&y2!ay_x8sx!=34#5q^sy%_vaog@9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['p5-walkthrough.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['p5-walkthrough.herokuapp.com', 'localhost']
 
 
 # Application definition
